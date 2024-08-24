b30ceac1201e97960099d33e013c8cbf181fabee
Arsenii Gorkin <gorkin@protonmail.com>
2024-08-24 08:37:35 +0300

    Рабочая 1

--- PROD/scripts/startup.sh
+++ PROD/scripts/startup.sh
@@ -15,50 +15,12 @@ function apache_start {
 }
 
 # Запуск Proton сервера
-# Сервис запускается дважды, так как с первого раза некорректно работает. Это - тайна какая-то :)
 function proton_start {
-  echo 'Первый "холодный" запуск сервиса Proton'
+  echo 'Запуск сервиса Proton'
   cd /var/www/distros/mediawiki-services-chromium-render &&
   
   # Запуск npm сервера в фоновом режиме. NB: нужно именно так запускать, чтобы его найти потом в ps aux, иначе будет только ../server.js, а их может быть несколько у нас в будущем
   node /var/www/distros/mediawiki-services-chromium-render/server.js &
-  
-  # Сохранение PID запущенного процесса npm (он запустит Node.js)
-  npm_pid=$!
-
-  # Ожидание полного запуска
-  echo 'Ожидаем полный старт 5 секунд...'
-  sleep 5
-  
-  # Получение реального PID процесса Node.js
-  node_pid=$(ps aux | grep 'node' | grep '/var/www/distros/mediawiki-services-chromium-render/server.js' | grep -v 'grep' | awk '{print $2}')
-
-  # Проверка наличия PID
-  if [ -n "$node_pid" ]; then
-      echo "PID Proton сервиса: $node_pid"
-  else
-      echo "Не удалось получить PID процесса Node.js"
-      kill -SIGTERM "$npm_pid"  # Попытка завершить процесс npm, если не найден node_pid
-      exit 1  # Завершить скрипт с ошибкой, если PID не найден
-  fi
-
-  # Ожидание сигнала для завершения
-  echo 'Остановка первого запуска сервиса Proton...'
-  kill -SIGTERM "$node_pid"  # Здесь нужно использовать $node_pid
-
-  # Проверка завершения процесса
-  sleep 2  # Дать время для завершения процесса
-  if kill -0 "$node_pid" 2>/dev/null; then  # Используйте $node_pid
-      echo "Процесс $node_pid не завершился корректно."
-      exit 1
-  else
-      echo "Процесс $node_pid успешно завершен."
-  fi
-
-  # Повторный запуск сервиса, который будет работать постоянно
-  echo 'Повторный запуск Proton сервиса...'
-  cd /var/www/distros/mediawiki-services-chromium-render &&
-  node /var/www/distros/mediawiki-services-chromium-render/server.js
 }
 
 
